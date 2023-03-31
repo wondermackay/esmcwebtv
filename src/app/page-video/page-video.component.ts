a7a100a2d4808cd9ade95d49e27b3bfffdfc30eb
Vlax <vlax.dev@gmail.com>
2023-03-31 18:27:44 +0000

    bbb

--- src/app/page-video/page-video.component.ts
+++ src/app/page-video/page-video.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-page-video',
   templateUrl: './page-video.component.html',
   styleUrls: ['./page-video.component.scss']
 })
-export class PageVideoComponent {
+export class PageVideoComponent implements OnInit {
+  ngOnInit() {
+    const api_key = "AIzaSyAuNzmHr2U2naJQJwm8LCyowx6PeEFNSTY";
+    const playlist_id = "PLsoi2D5rHj1yALMuMV3lZaCMUTEj2Wy4n";
 
+// Définition de la fonction asynchrone pour récupérer les IDs de toutes les vidéos de la playlist
+    async function getVideoIds(): Promise<string[]> {
+      let videoIds: string[] = [];
+      let nextPageToken = "";
+
+      // Tant qu'il y a une page suivante, continuer à appeler l'API YouTube Data
+      while (nextPageToken !== undefined) {
+        const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlist_id}&key=${api_key}&pageToken=${nextPageToken}`;
+        const response = await fetch(url);
+        const result = await response.json();
+
+        for (const item of result.items) {
+          const videoId = item.snippet.resourceId.videoId;
+          videoIds.push(videoId);
+        }
+
+        nextPageToken = result.nextPageToken;
+      }
+
+      return videoIds;
+    }
+
+// Appel de la fonction pour récupérer les IDs de toutes les vidéos de la playlist
+    getVideoIds().then((videoIds) => {
+      console.log(videoIds);
+    });
+  }
 }

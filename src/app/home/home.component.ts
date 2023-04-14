4d805adedfa47b1fbcb16a24a5100c6f8c231996
LeBug <95646927+FlexLevrai@users.noreply.github.com>
2023-04-14 21:05:04 +0000

    Vendredi 14 23

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {CardVideo} from 'src/app/models/card-video.model';
 import {ProgCard} from '../models/prog-card.model';
-import {ProgrammeComponent} from '../programme/programme.component';
 
 
 @Component({
@@ -83,67 +82,7 @@ export class HomeComponent implements OnInit{
     this.progCard4 = {
       progCardImg: './assets/image/b.webp',
       progCardTitle: 'Le Bâton de Moïse}',
-    }
-      ;
-
-
-
-      // // Appel des cartes des Vidéos
-
-      // this.cardVideo1 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre1',
-      //   '30:24',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-
-      // this.cardVideo2 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre2',
-      //   '30:24',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-
-      // this.cardVideo3 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre3',
-      //   '30:24',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-
-      // this.cardVideo4 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre4',
-      //   '30:24',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-
-      // this.cardVideo5 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre5',
-      //   '30:24',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-
-      // this.cardVideo6 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre6',
-      //   '30:24',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-
-      // this.cardVideo7 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre7',
-      //   '45:04',
-      //   'Lorem, ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
-      // this.cardVideo8 = new CardVideo(
-      //   'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-      //   'Titre8',
-      //   '15:54',
-      //   'ipsum dolor sit Lorem, ipsum dolor sit'
-      // );
+    };
     }
     constructor(private router: Router){}
   }

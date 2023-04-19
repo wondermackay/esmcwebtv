b718268dc2d57e1ad2e7f386e166463151122b4f
reineafola <alofaussert@gmail.com>
2023-04-19 16:39:29 +0000

    19-04-23

--- src/app/page-video/page-video.component.ts
+++ src/app/page-video/page-video.component.ts
@@ -1,120 +1,56 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { programmeModel } from '../models/programme.model';
-import ProgrammeJson from '../programme.json'
+import ProgrammeJson from '../programme.json';
 @Component({
   selector: 'app-page-video',
   templateUrl: './page-video.component.html',
-  styleUrls: ['./page-video.component.scss']
+  styleUrls: ['./page-video.component.scss'],
 })
 export class PageVideoComponent implements OnInit {
-
   title!: string | null;
   detprog: any;
-  id:number=0;
-  nom:String="";
-  photo:String="";
+  id: number = 0;
+  nom: String = '';
+  photo: String = '';
 
-  prog:programmeModel[] = ProgrammeJson
+  prog: programmeModel[] = ProgrammeJson;
   step: number = 0;
   loading: number = 1;
   detOdd: any;
-
-
-
-  tepisode !:any
+video!:any
+  tepisode!: any;
   cibleDataValue: any;
   tc: any;
   oddId = 0;
-  data:any
+  data: any;
   params: any;
-  // tepisode = 
-  //  tprog = [
-//   {
-//     id: 1,
-//     title:'Arche de Noé',
-//     img: "./assets/image/arch.webp",
-//   },
-//   {
-//     id: 2,
-//     title:'Le Jardin d Eden',
-//     img: "./assets/image/jardin.webp",
-//   },
-//   {
-//     id: 3,
-//     title:'Resurection',
-//     img: "./assets/image/resurection.webp",
-//   },
-//   {
-//     id: 4,
-//     title:'Le Bâton de Moïse',
-//     img: "./assets/image/b.webp",
-//   },
-//   {
-//     id: 5,
-//     title:'Le Bâton de Moïse',
-//     img: "./assets/image/b.webp",
-//   },
-//   {
-//     id: 6,
-//     title:'Resurection',
-//     img: "./assets/image/resurection.webp",
-//   },
-//   {
-//     id: 7,
-//     title:'Sceau des Prophetes',
-//     img: "./assets/image/sceau.webp",
-//   },
-//   {
-//     id: 8,
-//     title:'Mashia Messie Mahdi',
-//     img: "./assets/image/3m.webp",
-//   },
-//   {
-//     id: 9,
-//     title:'Jour de la Religion',
-//     img: "./assets/image/jourreligion.webp",
-//   },
-//   {
-//     id: 10,
-//     title:'Jour dernier',
-//     img: "./assets/image/jourdernier.webp",
-//   },
-//   {
-//     id: 11,
-//     title:'Resurection',
-//     img: "./assets/image/resurection.webp",
-//   },
-//   {
-//     id: 12,
-//     title:'Resurection',
-//     img: "./assets/image/resurection.webp",
-//   },
- 
-// ];
+ //videos: url =(https:player.viloud.tv/embed/channel/24f702c58ea642853ed4282d5c0cce45?autoplay=1&volume=1&controls=1&title=1&share=1&open_playlist=0&random=0)
+  
 
-  constructor(private route: ActivatedRoute,private router: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: ActivatedRoute) {}
   ngOnInit() {
-    this.params = this.router.snapshot.params
-    console.log("param",this.params)
-        this.id = this.router.snapshot.params["title"];
-    console.log("reine", this.id)
+    this.params = this.router.snapshot.params;
+    console.log('param', this.params);
+    this.id = this.router.snapshot.params['title'];
+    console.log('reine', this.id);
 
-    this.getDetailsProgramme(this.id)
-   // this.title = this.route.snapshot.paramMap.get('title');
-    const api_key = "AIzaSyAuNzmHr2U2naJQJwm8LCyowx6PeEFNSTY";
-    const playlist_id = "PLsoi2D5rHj1yALMuMV3lZaCMUTEj2Wy4n";
+    this.getDetailsProgramme(this.id);
+    // this.title = this.route.snapshot.paramMap.get('title');
+    const api_key = 'AIzaSyAuNzmHr2U2naJQJwm8LCyowx6PeEFNSTY';
+    const playlist_id = 'PLsoi2D5rHj1yALMuMV3lZaCMUTEj2Wy4n';
 
-// Définition de la fonction asynchrone pour récupérer les IDs de toutes les vidéos de la playlist
+    // Définition de la fonction asynchrone pour récupérer les IDs de toutes les vidéos de la playlist
     async function getVideoIds(): Promise<string[]> {
       let videoIds: string[] = [];
-      let nextPageToken = "";
+      let nextPageToken = '';
 
       // Tant qu'il y a une page suivante, continuer à appeler l'API YouTube Data
       while (nextPageToken !== undefined) {
         const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlist_id}&key=${api_key}&pageToken=${nextPageToken}`;
         const response = await fetch(url);
         const result = await response.json();
+        console.log("okiiii",result)
 
         for (const item of result.items) {
           const videoId = item.snippet.resourceId.videoId;
@@ -125,87 +61,89 @@ export class PageVideoComponent implements OnInit {
       }
 
       return videoIds;
-    }
 
-// Appel de la fonction pour récupérer les IDs de toutes les vidéos de la playlist
+    }
+    
+
+    // Appel de la fonction pour récupérer les IDs de toutes les vidéos de la playlist
     getVideoIds().then((videoIds) => {
-      console.log(videoIds);
+      this.video=videoIds
+      console.log("videoIds",this.video);
     });
   }
 
-getDetailsProgramme(id:number){
-  this.detprog = this.router.snapshot.params["title"];
-  if (id == 1) {
-    this.id = 1;
-    this.nom = this.prog[0].libelle;
-    this.photo = "./assets/images/odd/odd1.webp";
-    this.tepisode = this.prog[0].child
-  }
-  if (id == 2) {
-    this.id = 2;
-    this.nom = this.prog[1].libelle;
-    this.photo = "./assets/images/odd/odd2.webp";
-    this.tepisode = this.prog[1].child
-  }
-  if (id == 3) {
-    this.id = 3;
-    this.nom = this.prog[2].libelle;
-    this.photo = "./assets/images/odd/odd3.webp";
-    this.tepisode = this.prog[2].child
-  }
+  getDetailsProgramme(id: number) {
+    this.detprog = this.router.snapshot.params['title'];
+    if (id == 1) {
+      this.id = 1;
+      this.nom = this.prog[0].libelle;
+      this.photo="url(https:player.viloud.tv/embed/channel/24f702c58ea642853ed4282d5c0cce45?autoplay=1&volume=1&controls=1&title=1&share=1&open_playlist=0&random=0)"
+      this.tepisode = this.prog[0].child;
+    }
+    if (id == 2) {
+      this.id = 2;
+      this.nom = this.prog[1].libelle;
+      this.photo = './assets/images/odd/odd2.webp';
+      this.tepisode = this.prog[1].child;
+    }
+    if (id == 3) {
+      this.id = 3;
+      this.nom = this.prog[2].libelle;
+      this.photo = './assets/images/odd/odd3.webp';
+      this.tepisode = this.prog[2].child;
+    }
 
-  
-  if (id == 4) {
-    this.id = 4;
-    this.nom = this.prog[3].libelle;
-    this.photo = "./assets/images/odd/odd4.webp";
-    this.tepisode = this.prog[3].child
-  }
-  if (id == 5) {
-    this.id = 5;
-    this.nom = this.prog[4].libelle;
-    this.photo = "./assets/images/odd/odd5.webp";
-    this.tepisode = this.prog[4].child
-  }
-  if (id == 6) {
-    this.id = 6;
-    this.nom = this.prog[5].libelle;
-    this.photo = "./assets/images/odd/odd6.webp";
-    this.tepisode = this.prog[5].child
-  }
-  if (id == 7) {
-    this.id = 7;
-    this.nom = this.prog[6].libelle;
-    this.photo = "./assets/images/odd/odd7.webp";
-    this.tepisode = this.prog[6].child
-  }
-  if (id == 8) {
-    this.id = 8;
-    this.nom = this.prog[7].libelle;
-    this.photo = "./assets/images/odd/odd8.webp";
-    this.tepisode = this.prog[7].child
-  }
-  if (id == 9) {
-    this.id = 9;
-    this.nom = this.prog[8].libelle;
-    this.photo = "./assets/images/odd/odd9.webp";
-    this.tepisode = this.prog[8].child
-  }
-  if (id == 10) {
-    this.id = 10;
-    this.nom = this.prog[9].libelle;
-    this.photo = "./assets/images/odd/odd10.webp";
-    this.tepisode = this.prog[9].child
-  }
-  if (id == 11) {
-    this.id = 11;
-    this.nom = this.prog[10].libelle;
-    this.photo = "./assets/images/odd/odd11.webp";
-    this.tepisode = this.prog[10].child
-  }
- 
-}
+    if (id == 4) {
+      this.id = 4;
+      this.nom = this.prog[3].libelle;
+      this.photo = './assets/images/odd/odd4.webp';
+      this.tepisode = this.prog[3].child;
+    }
+    if (id == 5) {
+      this.id = 5;
+      this.nom = this.prog[4].libelle;
+      this.photo = './assets/images/odd/odd5.webp';
+      this.tepisode = this.prog[4].child;
+    }
+    if (id == 6) {
+      this.id = 6;
+      this.nom = this.prog[5].libelle;
+      this.photo = './assets/images/odd/odd6.webp';
+      this.tepisode = this.prog[5].child;
+    }
+    if (id == 7) {
+      this.id = 7;
+      this.nom = this.prog[6].libelle;
+      this.photo = './assets/images/odd/odd7.webp';
+      this.tepisode = this.prog[6].child;
+    }
+    if (id == 8) {
+      this.id = 8;
+      this.nom = this.prog[7].libelle;
+      this.photo = './assets/images/odd/odd8.webp';
+      this.tepisode = this.prog[7].child;
+    }
+    if (id == 9) {
+      this.id = 9;
+      this.nom = this.prog[8].libelle;
+      this.photo = './assets/images/odd/odd9.webp';
+      this.tepisode = this.prog[8].child;
+    }
+    if (id == 10) {
+      this.id = 10;
+      this.nom = this.prog[9].libelle;
+      this.photo = './assets/images/odd/odd10.webp';
+      this.tepisode = this.prog[9].child;
+    }
+    if (id == 11) {
+      this.id = 11;
+      this.nom = this.prog[10].libelle;
+      this.photo = './assets/images/odd/odd11.webp';
+      this.tepisode = this.prog[10].child;
+    }
 
-
-
+    console.log('ffeeeeeee', this.tepisode);
+    console.log('nom', this.nom);
+    // console.log("nom",this.nom)
+  }
 }
